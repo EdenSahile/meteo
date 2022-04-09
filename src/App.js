43681ff355d4a9d0095ab94da77975fe15f0f07a
Eden Sahilé <edensahile28@gmail.com>
2022-04-09 17:59:30 +0200

    implementation fetch method - api call check with console log

--- src/App.js
+++ src/App.js
@@ -8,9 +8,75 @@ import './App.css';
 
 export default class App extends Component {
 
+  constructor(props){
+
+    super(props)
+
+    this.state={
+
+      baseURL :'https://api.openweathermap.org/data/2.5/weather?',
+      APIkey:'f09a8654d3fc3b74ab1b6d8613069f0a',
+      input:'paris'
+
+    }
+  }
+
+
+
+  // componentDidMount(){
+  //   this.query()
+  // }
+
+  handleChange(e){
+    console.log(e.target.value)
+    this.setState({
+      input:e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    this.query()
+ 
+  }
+
+
+  query(){
+
+
+    let url =this.state.baseURL+"q="+this.state.input+"&appid="+this.state.APIkey
+
+
+    fetch(url)
+      .then(response=>{
+
+        if(!response.ok){
+          console.log('error');
+         
+        }
+
+       return response.json();
+      })
+      .then(data=>{
+        console.log(data)
+    
+  })
+
+}
+
   render() {
 
-    return()
+    return(
+
+        <div>
+      <SearchBar change={(e)=>this.handleChange(e)} submit={(e)=>this.handleSubmit(e)}/>
+
+      <Main/>
+      <Footer/>
+
+      </div>
+
+    )
 
  
    
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export const SearchBar = () => {
+export const SearchBar = ({
+
+  change,
+  submit
+}) => {
   return (
 
      <div id="current" className="wrapper">
 
          <nav className=" navbar-fixed-top">
             <div className="container margin-left" >
-              <form className="card my-6" >
+              <form className="card my-6" onSubmit={submit} >
                 <div className="card-body row no-gutters align-items-center">
 
                   <div className="col">
@@ -15,6 +19,7 @@ export const SearchBar = () => {
                     className="form-control form-control-lg form-control-borderless" 
                     type="search"
                     placeholder="Search ..."
+                    onChange={change}
                     
                       
                       />
